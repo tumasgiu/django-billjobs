fcca89b681a92036a7c1ca704f0edb0cb0cf2768
Lionel Chanson <github@lionelchanson.fr>
2017-06-30 16:08:51 +0200

    test(permission-api): test /permissions endpoint with GET method is not public

---
+++ billjobs/tests/tests_permission_api.py
@@ -0,0 +1,23 @@
+from django.urls import reverse
+from rest_framework import status
+from billjobs.tests.generics import GenericAPITest
+import collections
+
+class AnonymousPermissionAPITest(GenericAPITest):
+    """
+    Test /permissions endpoint status code and response data for anonymous user
+    """
+
+    def setUp(self):
+        super().setUp()
+        self.url = reverse('permissions-api')
+        self.expected_status = {
+                'GET': 401
+                }
+
+    def tearDown(self):
+        super().tearDown()
+
+    def test_permission_api_status_code(self):
+        self.status_code_is()
+
