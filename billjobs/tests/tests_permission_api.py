fabb2728e8cf2223c5fbcb994a93189d63c77dde
Lionel Chanson <github@lionelchanson.fr>
2017-06-30 16:19:11 +0200

    test(permission-api): test /permissions/pk endpoints with all HTTP method is not public

--- billjobs/tests/tests_permission_api.py
+++ billjobs/tests/tests_permission_api.py
@@ -38,3 +38,40 @@ class AnonymousPermissionAPITest(GenericAPITest):
 
     def test_permission_api_content(self):
         self.content_is()
+
+class AnonymousPermissionDetailAPITest(GenericAPITest):
+    """
+    Test /permissions/pk endpoint status code and response data for anonymous
+    user
+    """
+
+    def setUp(self):
+        super().setUp()
+        self.url = reverse('permissions-detail-api', args=(1,))
+        self.expected_status = {
+                'GET': 401,
+                'POST': 401,
+                'PUT': 401,
+                'DELETE': 401,
+                'HEAD': 401,
+                'OPTIONS': 401,
+                'PATCH': 401,
+                }
+        self.expected_content = {
+                'GET': self.error_message['401'],
+                'POST': self.error_message['401'],
+                'PUT': self.error_message['401'],
+                'DELETE': self.error_message['401'],
+                'HEAD': self.error_message['401'],
+                'OPTIONS': self.error_message['401'],
+                'PATCH': self.error_message['401'],
+                }
+
+    def tearDown(self):
+        super().tearDown()
+
+    def test_permission_detail_api_status_code(self):
+        self.status_code_is()
+
+    def test_permission_detail_api_content(self):
+        self.content_is()
